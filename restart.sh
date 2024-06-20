7f0169331d42e03d06de21e8d5a2187a75460f80
Your Name <your_email@example.com>
2024-06-20 22:45:59 +0500

    first

---
+++ restart.sh
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+sudo systemctl restart bot_beat.service
+sudo systemctl status bot_beat.service
+
+
+sudo systemctl restart bot_worker.service
+sudo systemctl status bot_worker.service
+
+
+sudo systemctl restart bot
+sudo systemctl status bot
