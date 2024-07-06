5c0d5369aac61506a47a3c263bc513912b6dd6c8
Your Name <your_email@example.com>
2024-07-06 11:46:40 +0500

    first

--- restart.sh
+++ restart.sh
@@ -8,13 +8,19 @@ sudo systemctl daemon-reload
 
 
 
+sudo systemctl start bot_beat.service
+sudo systemctl enable bot_worker.service
 sudo systemctl restart bot_beat.service
 sudo systemctl status bot_beat.service
 
 
+sudo systemctl start bot_worker.service
+sudo systemctl enable bot_worker.service
 sudo systemctl restart bot_worker.service
 sudo systemctl status bot_worker.service
 
 
+sudo systemctl start bot
+sudo systemctl enable bot
 sudo systemctl restart bot
 sudo systemctl status bot
