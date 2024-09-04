69e9d4a4c03d7d40549e1bde66c4ed7b5fdf8d6e
ginza <ginza@tuit.uz>
2024-09-05 03:13:55 +0500

    iii

--- bots/common_bot/bot.py
+++ bots/common_bot/bot.py
@@ -1,13 +1,13 @@
 from telegram.bot import Bot
 from .models import BotToken
-from django.db import OperationalError, DatabaseError
+from django.db import OperationalError, DatabaseError, ProgrammingError
 
 try:
     if BotToken.objects.exists():
         telegram_token = BotToken.objects.first().token
     else:
         telegram_token = "7015018136:AAEsRZOz4CN8pRwu56vmLJRbe23IyALWoig"
-except (OperationalError, DatabaseError):
+except (OperationalError, DatabaseError, ProgrammingError):
     # Handle the case where the table does not exist or there is another database error
     telegram_token = "7015018136:AAEsRZOz4CN8pRwu56vmLJRbe23IyALWoig"
 
