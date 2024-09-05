5292865f5010013cc72e53bb33b32b51d32f011e
ginza <ginza@tuit.uz>
2024-09-06 00:23:04 +0500

    iii

--- bots/common_bot/bot.py
+++ bots/common_bot/bot.py
@@ -3,11 +3,11 @@ from .models import BotToken
 from django.db import OperationalError, DatabaseError, ProgrammingError
 telegram_token = "7015018136:AAEsRZOz4CN8pRwu56vmLJRbe23IyALWoig"  # Default token
 #
-try:
-    if BotToken.objects.exists():
-        telegram_token = BotToken.objects.first().token
-except (OperationalError, DatabaseError, ProgrammingError):
-    # Ignore the error and continue using the default token if table doesn't exist or there's any error
-    pass
+# try:
+#     if BotToken.objects.exists():
+#         telegram_token = BotToken.objects.first().token
+# except (OperationalError, DatabaseError, ProgrammingError):
+#     # Ignore the error and continue using the default token if table doesn't exist or there's any error
+#     pass
 
 bot_dp = Bot(telegram_token)
