5b908acda9927696d95712d7341282f7502decc6
ginza <ginza@tuit.uz>
2024-09-19 00:19:08 +0500

    iii

--- bots/common_bot/default_handlers/onboarding/handlers.py
+++ bots/common_bot/default_handlers/onboarding/handlers.py
@@ -14,20 +14,23 @@ from ...models import User
 
 def command_start(update: Update, context: CallbackContext) -> None:
     u, created = User.get_user_and_created(update, context)
+    lang=u.selected_language
     if created:
         text = static_text.start_created.format(first_name=u.first_name)
     else:
-        text = static_text.start_not_created.format(first_name=u.first_name)
+        text = static_text.start_not_created[lang].format(first_name=u.first_name)
 
     update.message.reply_text(text=text,
                               reply_markup=make_keyboard_for_start_command())
 
 
 def secret_level(update: Update, context: CallbackContext) -> None:
+    u, created = User.get_user_and_created(update, context)
+    lang=u.selected_language
     # callback_data: SECRET_LEVEL_BUTTON variable from manage_data.py
     """ Pressed 'secret_level_button_text' after /start command"""
     user_id = extract_user_data_from_update(update)['user_id']
-    text = static_text.unlock_secret_room.format(
+    text = static_text.unlock_secret_room[lang].format(
         user_count=User.objects.count(),
         active_24=User.objects.filter(updated_at__gte=timezone.now() - datetime.timedelta(hours=24)).count()
     )
